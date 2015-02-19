6affc98f98bf9c1b1a68e990719627da404a01a4
Åke <ake@burden.se>
2015-02-19 11:30:30 +0100

    Skeleton

---
+++ se/hig/student/hfk10aek/source/DataSource.py
@@ -0,0 +1,17 @@
+'''
+Created on 19 feb 2015
+
+@author: Åke
+'''
+
+class DataSource(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
